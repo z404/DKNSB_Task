91f951fc7068ea177f8b608b8f05142db4ce49ce
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2022-11-17 22:57:41 +0530

    created basic frontend app

---
+++ frontend_app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+st.title('Programming Task for DKNSB Medical Device Consultants')
+
+st.write('This simple frontend was built to create an interactive testing environment for the backend of the programming task.')
+
+#file upload
+st.write('Upload a pdf file to test the backend.')
+uploaded_file = st.file_uploader("Choose a file", type="pdf")
+
+#when file is uploaded, make post request to backend
+if uploaded_file is not None:
+    files = {'pdf': uploaded_file.getvalue()}
+    response = requests.post('http://localhost:8000/pdf', files=files)
+    print(response.json())
+
+    #display results
+    st.write('Results:')
+    df = pd.DataFrame(response.json()['message'])
+    st.write(df)
+
+#upload image
+st.write('Upload an image to test the backend.')
+uploaded_image = st.file_uploader("Choose an image", type=["jpg", "png"])
+
+#when image is uploaded, make post request to backend
+if uploaded_image is not None:
+    files = {'image': uploaded_image.getvalue()}
+    response = requests.post('http://localhost:8000/image', files=files)
+    print(response.json())
+
+    #display results
+    st.write('Results:')
+    df = pd.DataFrame(response.json()['message'], index=[0])
+    st.write(df)
