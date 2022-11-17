09347d00b5dced9aa1015919e4bc08d8246c8abd
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2022-11-17 20:34:28 +0530

    improved symbol identification

--- tests/SymbolTest.py
+++ tests/SymbolTest.py
@@ -1,26 +1,36 @@
 import cv2
-
+import numpy as np
 from skimage.feature import match_template
 from skimage.color import rgb2gray
 
 img1 = cv2.imread('./images/result_Page_1.jpg')
-img2 = cv2.imread('./symbols/9.png')[:,:,:3]
 
 # make img 1 a square by increasing the height
 img1 = cv2.resize(img1, (img1.shape[1], img1.shape[1]))
 
-# save the image
-cv2.imwrite('./res/resize.jpg', img1)
+height, width = 175, 210
+template_rows = [800, 800+height]
+template_cols = [150, 150+width, 150+2*width, 150+3*width]
 
-# match the images
-result = match_template(rgb2gray(img1), rgb2gray(img2))
+# read symols directory
+symbols = []
+for i in range(1, 10):
+    symbols.append(cv2.imread('./symbols/{}.png'.format(i))[:,:,:3])
 
-# get the best match
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-print(max_val)
+for row_num, row_val in enumerate(template_rows):
+    for col_num, col_val in enumerate(template_cols):
+        template = img1[row_val - 75:row_val+height + 75, col_val - 75:col_val+width + 75]
+        template_crop = template[75:-75, 75:-75]
+        if np.mean(template_crop) > 245:
+            continue
+        
 
-# draw a rectangle around the matched region
-cv2.rectangle(img1, max_loc, (max_loc[0] + img2.shape[1], max_loc[1] + img2.shape[0]), (0, 255, 0), 2)
-
-# show the result
-cv2.imwrite('./res/result_Page_x2.jpg', img1)
+        max_temp_score, max_temp = 0, 0
+        for symbol_num, symbol in enumerate(symbols):
+            result = cv2.matchTemplate(template, symbol, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            if max_val > max_temp_score:
+                max_temp_score = max_val
+                max_temp = symbol_num
+        print('row: {}, col: {}, max_temp: {}'.format(row_num, col_num, max_temp + 1))
+        
